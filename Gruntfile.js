500e647bd069f40771340929ddcf19d5e196cbc4
Joshua Thomas <jthoms1@gmail.com>
2013-09-02 23:22:55 -0500

    Changed gruntfile to be more friendly for changes.  Removed amd compliant versions of backbone and underscore in place of using shims from requirejs. Updated bower to use bowerrc file and bower.json in place of component.json.  Removed jshint options from gruntfile and moved into a .jshint file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,17 @@
 /*global module:false*/
-'use strict';
 var path = require("path");
 
 module.exports = function (grunt) {
-
     var init = {
+        dir: {
+            bower_components:  grunt.file.readJSON('.bowerrc').directory + "/",
+            dev:               'dev/',
+            prod:              'publish/',
+            source:            'src/',
+            js_code:           'src/js/',
+            css_code:          'src/css/',
+            tpls:              'src/js/templates'
+        },
         pkg: grunt.file.readJSON('package.json'),
         /**
          * Lints javascript files using jshint
@@ -14,61 +21,27 @@ module.exports = function (grunt) {
          */
         jshint: {
             files: [
-                'src/js/!(templates).js'
+                '<%= dir.js_code %>!(templates).js'
             ],
-            options: {
-                browser: true,
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                indent: 4,
-                plusplus: true,
-                latedef: true,
-                undef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                eqnull: true,
-                strict: true,
-                trailing: true,
-                globals: {
-                    jQuery: true,
-                    require: true,
-                    define: true,
-                    JST: true
-                }
-            }
+            jshintrc: ".jshintrc"
         },
 
         /*
-         * RECESS
+         * LESS
          * (grunt-less)
-         * Default options
-         * ----------------------
-         *     compile: false             // Compiles CSS or LESS. Fixes white space and sort order.
-         *     compress: false            // Compress your compiled code
-         *     noIDs: true                // Doesn't complain about using IDs in your stylesheets
-         *     noJSPrefix: true           // Doesn't complain about styling .js- prefixed classnames
-         *     noOverqualifying: true     // Doesn't complain about overqualified selectors (ie: div#foo.bar)
-         *     noUnderscores: true        // Doesn't complain about using underscores in your class names
-         *     noUniversalSelectors: true // Doesn't complain about using the universal * selector
-         *     prefixWhitespace: true     // Adds whitespace prefix to line up vender prefixed properties
-         *     strictPropertyOrder: true  // Complains if not strict property order
-         *     stripColors: false         // Strip colors from the Terminal output
-         *     zeroUnits: true            // Doesn't complain if you add units to values of 0
          */
         less: {
             // Only need to compile files when working in development environment
             dev: {
                 files: {
-                    'dev/css/base.css': 'src/css/base.less'
+                    '<%= dir.dev %>css/base.css': '<%= dir.css_code %>base.less'
                 }
             },
 
             // When compiling less files for production also minify them(compress)
             prod: {
                 files: {
-                    'publish/css/base.css': 'src/css/base.less'
+                    '<%= dir.prod %>css/base.css': '<%= dir.css_code %>base.less'
                 },
                 options: {
                     compress: true
@@ -76,21 +49,51 @@ module.exports = function (grunt) {
             }
         },
 
+        /*
+         * RequireJS: Concat and compile if prod
+         */
         requirejs: {
-            compile: {
-                options: {
-                    name: "main",
-                    baseUrl: "./src/js",
-                    paths: {
-                        "jquery"                    : "../../components/jquery/jquery",
-                        "backbone"                  : "../../components/backbone-amd/backbone",
-                        "underscore"                : "../../components/underscore-amd/underscore",
-                        "lib/backbone/localstorage" : "../../components/backbone.localStorage/backbone.localStorage"
+            options: {
+                name: '../../<%= dir.bower_components %>/almond/almond',
+                include: "main",
+                baseUrl: "<%= dir.js_code %>",
+                paths: {
+                    "jquery"                    : "../../<%= dir.bower_components %>/jquery/jquery",
+                    "backbone"                  : "../../<%= dir.bower_components %>/backbone/backbone",
+                    "underscore"                : "../../<%= dir.bower_components %>/underscore/underscore",
+                    "lib/backbone/localstorage" : "../../<%= dir.bower_components %>/backbone.localStorage/backbone.localStorage"
+                },
+                shim: {
+                    "underscore" : {
+                        exports: "_"
                     },
-                    shim: {},
-                    optimize: "none",
-                    mainConfigFile: "src/js/main.js",
-                    out: "dev/js/global.js"
+                    "backone": {
+                        exports: "Backbone"
+                    },
+                    "lib/backbone/localstorage": {
+                        exports: "Backbone"
+                    }
+                },
+                mainConfigFile: "<%= dir.js_code %>main.js"
+            },
+            dev: {
+                options: {
+                    out: "<%= dir.dev %>js/main.js",
+                    optimize: "none"
+                }
+            },
+            prod: {
+                options: {
+                    out: "<%= dir.prod %>js/main.js",
+                    uglify: {
+                        toplevel: true,
+                        ascii_only: true,
+                        max_line_length: 1000,
+                        defines: {
+                            DEBUG: ['name', 'false']
+                        },
+                        no_mangle: true
+                    }
                 }
             }
         },
@@ -103,14 +106,14 @@ module.exports = function (grunt) {
             compile: {
                 options: {
                     amd: true,
-                    relativePath: "src/js/templates/",
+                    relativePath: "<%= dir.tpls %>",
                     processName: function (src) {
-                        var relativePath = "src/js/templates/";
+                        var relativePath = "<%= dir.js_code %>templates/";
                         return src.replace(/(\.[^.]+)$/, "").replace(relativePath, ""); // Remove file extensions and relativePath
                     }
                 },
                 files: {
-                    'src/js/templates.js': ['<%= jst.compile.options.relativePath %>/**/*.html']
+                    '<%= dir.js_code %>templates.js': ['<%= dir.tpls %>**/*.html']
                 }
             }
         },
@@ -125,10 +128,10 @@ module.exports = function (grunt) {
                     basePath: "."
                 },
                 files: [
-                    {expand: true, cwd: 'src/', src: 'img/*', dest: 'dev/'},
-                    {expand: true, cwd: 'src/', src: 'index.html', dest: 'dev/'},
-                    {expand: true, cwd: 'components/html5shiv/src/', src: 'html5shiv.js', dest: 'dev/assets/'},
-                    {expand: true, cwd: 'components/requirejs/', src: 'require.js', dest: 'dev/assets/'}
+                    {expand: true, cwd: '<%= dir.src %>', src: 'img/*', dest: '<%= dir.dev %>'},
+                    {expand: true, cwd: '<%= dir.src %>', src: 'index.html', dest: '<%= dir.dev %>'},
+                    {expand: true, cwd: '<%= dir.bower_components %>html5shiv/src/', src: 'html5shiv.js', dest: '<%= dir.dev %>assets/'},
+                    {expand: true, cwd: '<%= dir.bower_components %>requirejs/', src: 'require.js', dest: '<%= dir.dev %>assets/'}
                 ]
             },
             prod: {
@@ -136,10 +139,10 @@ module.exports = function (grunt) {
                     basePath: "."
                 },
                 files: [
-                    {expand: true, cwd: 'src/', src: 'img/*', dest: 'publish/'},
-                    {expand: true, cwd: 'src/', src: 'index.html', dest: 'publish/'},
-                    {expand: true, cwd: 'components/html5shiv/src/', src: 'html5shiv.js', dest: 'publish/assets/'},
-                    {expand: true, cwd: 'components/requirejs/', src: 'require.js', dest: 'publish/assets/'}
+                    {expand: true, cwd: '<%= dir.src %>', src: 'img/*', dest: '<%= dir.prod %>'},
+                    {expand: true, cwd: '<%= dir.src %>', src: 'index.html', dest: '<%= dir.prod %>'},
+                    {expand: true, cwd: '<%= dir.bower_components %>html5shiv/src/', src: 'html5shiv.js', dest: '<%= dir.prod %>assets/'},
+                    {expand: true, cwd: '<%= dir.bower_components %>requirejs/', src: 'require.js', dest: '<%= dir.dev %>assets/'}
                 ]
             }
         },
@@ -148,29 +151,29 @@ module.exports = function (grunt) {
         uglify: {
             prod: {
                 files: {
-                    'publish/js/global.js': ['dev/js/global.js']
+                    '<%= dir.prod %>js/global.js': ['<%= dir.dev %>js/global.js']
                 }
             }
         },
 
         clean: {
             prod: {
-                src: ['publish']
+                src: ['<%= dir.prod %>']
             }
         },
 
         // Watch to concat js files and concat/compile less(css) files
         watch: {
             less : {
-                files: 'src/css/**/*.less',
+                files: '<%= dir.css_code %>**/*.less',
                 tasks: ['less:dev']
             },
             requirejs : {
-                files: ['src/js/**/*.js'],
+                files: ['<%= dir.js_code %>**/*.js'],
                 tasks: ['requirejs']
             },
             jst : {
-                files: 'src/js/templates/**/*.html',
+                files: '<%= dir.tpls %>/**/*.html',
                 tasks: ['jst', 'requirejs']
             }
         }
@@ -187,7 +190,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-devtools');
 
     /** default includes: compiling js templates, 
      *                    compiling/concating js files with requirejs
